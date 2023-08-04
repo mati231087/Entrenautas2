7415d577d0b6d6d39aea72ed9b632b0dc8ac52f0
mati231087 <matias.cereghetti@icloud.com>
2023-08-04 14:03:10 -0300

    cambios en views administrador

--- Project/apps/administrador/views.py
+++ Project/apps/administrador/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 
 def login_view(request):
     if request.method == "POST":
